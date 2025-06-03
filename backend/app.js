4ae8a7c86dbf1e6c2702fc3c87c02b0f6affdd11
SayuruDev <mailtosayuru@gmail.com>
2025-06-03 14:16:38 -0400

    Nodemon running

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 import express from "express";
 const app = express();
 import dotenv from "dotenv";
-
 dotenv.config({ path: "backend/config/config.env" });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on PORT: ${process.env.PORT}`);
+  console.log(`SERVER started on PORT: ${process.env.PORT}`);
 });
