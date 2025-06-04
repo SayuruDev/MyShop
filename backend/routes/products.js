869cd6c817c4d887940b60469163d8ab72be0aec
SayuruDev <mailtosayuru@gmail.com>
2025-06-03 21:49:58 -0400

    Connected to MongoDB local

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { getProducts } from "../controllers/productControllers.js";
 const router = express.Router();
-
-router.get("/products", getProducts);
+router.route("/products").get(getProducts);
 
 export default router;
