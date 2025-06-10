a8a2a575f2cfbd2c71668ca12470ef8773758f3b
SayuruDev <mailtosayuru@gmail.com>
2025-06-10 11:50:34 -0400

    35.Logout user + Set Cookie expire now

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { loginUser,registerUser } from '../controllers/authControllers.js';
+import { loginUser,registerUser,logout } from '../controllers/authControllers.js';
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").get(logout);
 export default router;
