fd426352778f18cde6e18faa7a060a9f55218461
SayuruDev <mailtosayuru@gmail.com>
2025-06-09 20:19:04 -0400

    32.Loginuser+Token

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import User from "../models/user.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 // Register user   =>  /api/v1/register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -16,4 +17,34 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
   res.status(201).json({
     token,
   })
+});
+
+// Login user   =>  /api/v1/login
+export const loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password", 400));
+  }
+
+  // Find user in the database
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  // Check if password is correct
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const token = user.getJwtToken();
+
+  res.status(201).json({
+    token,
+  })
+  
 });
--- backend/models/user.js
+++ backend/models/user.js
@@ -49,5 +49,9 @@ userSchema.methods.getJwtToken = function () {
   });
 };
 
+// Compare user password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export default mongoose.model("User", userSchema);
