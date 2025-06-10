2f07faf8f0bfe7306a1dc96af6e46be4bb4d89f2
SayuruDev <mailtosayuru@gmail.com>
2025-06-10 12:58:03 -0400

    36.login as admin to get the data

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -6,16 +6,18 @@ import {
   newProduct,
   updateProduct,
 } from "../controllers/productControllers.js";
-import { isAuthenticatedUser } from "../middlewares/auth.js";
-
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
-router.route("/products").get(isAuthenticatedUser,getProducts);
+router.route("/products")
+.get(isAuthenticatedUser,authorizeRoles("admin"),getProducts);
+
 router.route("/admin/products").post(newProduct);
 
 router.route("/products/:id").get(getProductDetails);
-router.route("/products/:id").put(updateProduct);
-router.route("/products/:id").delete(deleteProduct);
+
+router.route("/admin/products/:id").put(updateProduct);
+router.route("/admin/products/:id").delete(deleteProduct);
 
 export default router;
 
