34809a28bd1c40cbd7a0e2f82b593870c7707550
SayuruDev <mailtosayuru@gmail.com>
2025-06-14 18:17:49 -0400

    40.Send Password Recovery - Not working properly 41-42 skipped

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { loginUser,registerUser,logout } from '../controllers/authControllers.js';
+import { forgotPassword,loginUser,registerUser,logout } from '../controllers/authControllers.js';
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
+
+router.route("/password/forgot").post(forgotPassword);
+
 export default router;
