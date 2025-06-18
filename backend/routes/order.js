144011068ee361e8bfb6f89e46c81d67a983a65f
SayuruDev <mailtosayuru@gmail.com>
2025-06-17 21:55:32 -0400

    50.Get single Order-User Orders

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import { isAuthenticatedUser } from "../middlewares/auth.js";
-import {newOrder} from "../controllers/orderControllers.js";
+import {myOrders,newOrder,getOrderDetails} from "../controllers/orderControllers.js";
 router.route("/orders/new").post(isAuthenticatedUser, newOrder);
+router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
+router.route("/me/orders").get(isAuthenticatedUser, myOrders);
 
 export default router;
