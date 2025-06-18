fef3da11b3cc41314a17e5fd484794cb5fe4bd3b
SayuruDev <mailtosayuru@gmail.com>
2025-06-17 22:25:58 -0400

    50.Admin-Get/Update -Orders

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -57,3 +57,44 @@ export const myOrders = catchAsyncErrors(async (req, res, next) => {
     orders,
   });
 });
+
+// Get all orders - ADMIN  =>  /api/v1/admin/orders
+export const allOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  res.status(200).json({
+    orders,
+  });
+});
+
+// Update Order - ADMIN  =>  /api/v1/admin/orders/:id
+export const updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("No Order found with this ID", 404));
+  }
+
+  if (order?.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  // Update products stock
+  order?.orderItems?.forEach(async (item) => {
+    const product = await Product.findById(item?.product?.toString());
+    if (!product) {
+      return next(new ErrorHandler("No Product found with this ID", 404));
+    }
+    product.stock = product.stock - item.quantity;
+    await product.save({ validateBeforeSave: false });
+  });
+
+  order.orderStatus = req.body.status;
+  order.deliveredAt = Date.now();
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+  });
+});
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,17 @@
 import express from "express";
 const router = express.Router();
 
-import { isAuthenticatedUser } from "../middlewares/auth.js";
-import {myOrders,newOrder,getOrderDetails} from "../controllers/orderControllers.js";
+import {authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
+import {updateOrder,allOrders,myOrders,newOrder,getOrderDetails} from "../controllers/orderControllers.js";
 router.route("/orders/new").post(isAuthenticatedUser, newOrder);
 router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
 router.route("/me/orders").get(isAuthenticatedUser, myOrders);
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), allOrders);
+
+router
+  .route("/admin/orders/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
 
 export default router;
