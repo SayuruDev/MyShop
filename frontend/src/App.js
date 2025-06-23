06f4d2db8b659778a474d1e20b3f9424a2ed9453
SayuruDev <mailtosayuru@gmail.com>
2025-06-23 10:29:39 -0400

    60.Routing Implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Home from "./components/Home";
 
 function App() {
   return (
+    <Router>
     <div className="App">
      
        <Header />
            <div className="container">
-             <Home/>
+             <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
            </div>
       
        <Footer />
    
     </div>
+    </Router>
   );
 }
 
