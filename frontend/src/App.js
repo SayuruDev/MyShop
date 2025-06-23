fba2465eb724dab95eccf5c397499916c8733d35
SayuruDev <mailtosayuru@gmail.com>
2025-06-23 10:07:10 -0400

    58.Header -Footer Component Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-
+import "./App.css";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Shop IT</h1>
+       <Header />
+          <Footer />
+   
     </div>
   );
 }
