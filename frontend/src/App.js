6efe45f6a9f183008c06d6321d4f48fe60224441
SayuruDev <mailtosayuru@gmail.com>
2025-06-23 10:18:22 -0400

    59.Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
-
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
+     
        <Header />
-          <Footer />
+           <div className="container">
+             <Home/>
+           </div>
+      
+       <Footer />
    
     </div>
   );
