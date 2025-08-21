9bd9170571e3bf16b685c704e24f34d7bfc1541d
SayuruDev <mailtosayuru@gmail.com>
2025-08-21 17:15:56 -0400

    Dispaly All orders in Admin Dashboard

--- frontend/src/components/routes/adminRoutes.jsx
+++ frontend/src/components/routes/adminRoutes.jsx
@@ -6,6 +6,8 @@ import ListProducts from "../admin/ListProducts";
 import NewProduct from "../admin/NewProduct";
 import UpdateProduct from "../admin/UpdateProduct";
 import UploadImages from "../admin/UploadImages";
+import ListOrders from "../admin/ListOrders";
+
 
 const adminRoutes = () => {
   return (
@@ -50,6 +52,17 @@ const adminRoutes = () => {
           </ProtectedRoute>
         }
       />
+
+      <Route
+        path="/admin/orders"
+        element={
+          <ProtectedRoute admin={true}>
+            <ListOrders />
+          </ProtectedRoute>
+        }
+      />
+
+      
     </>
   );
 };
